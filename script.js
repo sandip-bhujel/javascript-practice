675e5a8725ec26faa3598176149dc69078a82a25
sandip-bhujel <bhujelsandip610@gmail.com>
2025-08-24 21:45:09 +0900

    practice datatypes with example

---
+++ script.js
@@ -0,0 +1,29 @@
+//Datatypes //
+//string//
+let myName = "sandip bhujel";
+console.log("my name is a :", myName);
+
+let address = "kathmandu dhobikhola -12";
+console.log("my home address is now", address);
+
+//number//
+let myAge = 35;
+console.log("my age is now", myAge, "year old");
+
+let blockNo = 2567;
+console.log("my home block number is", blockNo);
+
+//bolean//
+let isDriver = true;
+console.log("can i drive a new car?", isDriver);
+
+let isCook = false;
+console.log("he is not a good at cook food", isCook);
+
+//null//
+let cat = "null";
+console.log("cat", cat);
+
+//undefined//
+let future;
+console.log("my future is", future);
