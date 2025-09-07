49bc3676980f35f71a2f28dc47a9854bfd6fb4a0
sandip-bhujel <bhujelsandip610@gmail.com>
2025-09-07 09:29:32 +0900

    logical operator and or

---
+++ logical-operators-&&-II-!/script.js
@@ -0,0 +1,17 @@
+//&&//
+// both are true then return also true//
+//in movie hall can enter get ticket only age is 18 over //
+let age = 18;
+let canEnter = 22;
+console.log(age >= 22 && canEnter);
+
+// using OR || its a just one condition is true then it return true
+// if you are a student get a discount//
+let isStudent = true;
+let getDiscount = false;
+console.log(isStudent || getDiscount);
+
+// using ! NOT is it reverses value of condition//
+// when light is on the switch is NOT of//
+let isLightOn = true;
+console.log(!isLightOn);
