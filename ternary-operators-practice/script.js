7f9a4187fcf3b47b3cb90d2b8e9192055932b18d
sandip-bhujel <bhujelsandip610@gmail.com>
2025-09-12 23:15:42 +0900

    ternary operator practice some example

---
+++ ternary-operators-practice/script.js
@@ -0,0 +1,42 @@
+//even and odd numbers using ternary operators//
+
+// let num = 13;
+// let result = num % 2 === 0 ? "even" : "odd";   ------ short form of if else use gardako satto----
+// console.log(result);
+// -----------------
+// let num = 19;
+// let result;
+// if (num % 2 === 0) {
+//   result = "Even";
+// } else {
+//   result = "Odd";
+// }
+// console.log(result);
+
+// let function check(numb){
+//     return numb % 2 === 0? "even" : "odd";
+// }
+//fully if else using method but all same jun method le garda pani huncha
+let math = 180;
+let result;
+if (math % 2 === 0) {
+  result = "Even";
+} else {
+  result = "odd";
+}
+console.log(result);
+
+// ---------------------------
+
+//Adult and minor check using ternary method
+// let age = 34;
+// let pattern = age >= 18 ? "adult " : "minor";
+// console.log(pattern);
+
+let age = 34;
+if (age >= 18) {
+  pattern = "adult";
+} else {
+  pattern = "minor";
+}
+console.log(pattern);
