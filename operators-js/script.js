b866eee4ab30482b6162e96c31af9d8d00f17403
sandip-bhujel <bhujelsandip610@gmail.com>
2025-09-19 21:56:50 +0900

    arrithmetic unary modulus assignment operators

---
+++ operators-js/script.js
@@ -0,0 +1,40 @@
+//operators//
+//Arrithmetic operators//
+// let a = 10;
+// let b = 6;
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+
+//modulus  % //
+// console.log("a % b = ", a % b);
+
+//unary operators //
+//increment operator----- ++
+//decrement operator----- --
+// let a = 10;
+// let b = 6;
+// a = a + 1;  another write way is
+// a++;   (both are okay a++ , a--)
+//a++ ---- post
+//++a ---- pre
+// a--;
+// ++a; a++; same things
+// b--;
+// console.log("a = ", a);
+// console.log("b = ", b);
+// console.log("a++ = ", a++);
+// console.log("a =", a);
+
+//Assignment operstors ---------
+
+let a = 10;
+let b = 6;
+
+// a += 6; // a = a + 6 ===> a ko value is 10 cha aba add huncha 16 huncha //
+// a -= 6; // a = a - 6 ===> yesma minus huncha decrease
+a %= 4;
+a **= 4;
+
+console.log("a =", a);
