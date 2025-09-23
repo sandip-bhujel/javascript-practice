19835f04c1b50f99813f3f358a52e42d83af5f6d
sandip-bhujel <bhujelsandip610@gmail.com>
2025-09-23 22:04:41 +0900

    assignment operators practice short js

---
+++ operators-js/assignment-operators/script.js
@@ -0,0 +1,27 @@
+//assignment operators
+//=  +=  -+  *= %= **=
+
+//for example
+// let a = 4;
+// let b = 6;
+// a += a = a + 1;
+
+// a += 5;
+// a = a + 5; yesto bancha back ma detail
+
+// a -= 7;
+// a = a + 7;
+
+// a *= 8;
+// a %= 4;
+// a /= 3;
+// console.log("a = ", a);
+// console.log("a = ", a);
+
+//comparison operators
+//equal to == , not equal to != , equal to and type === , not equal to and type !==
+let a = 4;
+let b = 5;
+console.log("4 != 5", a != b);
+console.log("4 == 5", a == b);
+console.log("4 === 5", a === b);
