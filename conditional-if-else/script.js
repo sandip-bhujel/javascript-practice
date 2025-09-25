6a56e2252662b3752efff95bf27416522aa0856b
sandip-bhujel <bhujelsandip610@gmail.com>
2025-09-25 23:01:17 +0900

    conditional- if-else statement practice js

---
+++ conditional-if-else/script.js
@@ -0,0 +1,70 @@
+//conditional statement condition check garna ko lagi use garincha //
+//if statement //
+// let color;
+// if (mode === "dark-mode") {
+//   color = "black";
+// }
+
+// let age = 28;
+// if (age >= 16) {
+//   console.log("can go out");
+// }
+// if (age < 16) {
+//   console.log("can not go out");
+// }
+
+// let myNumber = 34567;
+// if (myNumber > 123) {
+//   console.log("wrong number");
+// }
+
+// let mode = "dark";
+// let color;
+// if (mode === "dark") {
+//   color = "black";
+// }
+// if (mode === "white") {
+//   color = "white";
+// }
+// console.log(color);
+
+// let drinks = "whiskey ";
+// let drink;
+// if (drinks === "whiskey") {
+//   drink = "hard jamney";
+// }
+// if (drinks === "beer") {
+//   drink = "soft bhayo ni bro";
+// }
+// console.log(drinks);
+
+//===============================
+
+//if/else statement start here
+let color = "white";
+if (color === "red") {
+  console.log("color red is perfect");
+} else {
+  console.log("not perfect others");
+}
+
+let age = 19;
+if (age >= 18) {
+  console.log("can vote");
+} else {
+  console.log("can not vote ");
+}
+
+let number = 18;
+if (number % 2 === 0) {
+  console.log(number, " is even numbers");
+} else {
+  console.log("odd number yeta");
+}
+
+let fruits = "orange";
+if (fruits === "banana") {
+  console.log("yo mitho hola");
+} else {
+  console.log("navaye namitho");
+}
