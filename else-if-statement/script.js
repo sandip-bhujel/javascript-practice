806b49a73526613c167cc45789b22438ee25d221
sandip-bhujel <bhujelsandip610@gmail.com>
2025-10-04 23:25:30 +0900

    practicing else-if js with examples

---
+++ else-if-statement/script.js
@@ -0,0 +1,24 @@
+//else-if statement //
+//we can use else if for multiple check or compare //
+let age = 30;
+if (age < 17) {
+  console.log("you are very small age for now");
+} else if (age < 20) {
+  console.log("you are tenager so you can add others");
+} else if (age < 50) {
+  console.log("you are mature person and adult too ");
+} else {
+  console.log("you can get real citizenship ");
+}
+
+//another example//
+let mode = "dark";
+if (mode === "dark") {
+  console.log("black");
+} else if (mode === "blue") {
+  console.log("color blue");
+} else if (mode === "green") {
+  console.log("color green");
+} else {
+  console.log("white");
+}
