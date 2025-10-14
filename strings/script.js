c7adfd6cb57740ae3838f4198d93a18ca225baab
sandip-bhujel <bhujelsandip610@gmail.com>
2025-10-14 23:05:48 +0900

    practicing string part create str literal length like this

---
+++ strings/script.js
@@ -0,0 +1,21 @@
+//create a string//
+let str = "hello";
+let str2 = "world";
+
+let str3 = "hamro ghar";
+console.log(str3[3]);
+
+let str6 = "banana";
+console.log(str6[2]);
+
+//template literals//
+let name = `mero name sandip bhujel ho `; //bactik ma rakheko aru kehi add garda dollar sign ani {}rakhna sakincha
+console.log(name);
+
+console.log("mero name\nsandip ho"); //      /n use grda break huncha same line ko tala jancha //
+console.log("ram\tbahadur"); //          /t le chai space tab dincha
+
+//string length//
+//str.length
+let personName = "ram bahadur gurung"; //18 but space also count in here
+console.log(personName.length);
