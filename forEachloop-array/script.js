c87b523b1ea65bbd9aacc1c4179961569f0d2488
sandip-bhujel <bhujelsandip610@gmail.com>
2025-10-31 23:22:42 +0900

    practice of function for each loop in array

---
+++ forEachloop-array/script.js
@@ -0,0 +1,35 @@
+//foreach loop //
+//array.foreach(callback function)
+let fruits = ["mango", "apple", "banana", "kiwi", "orange"];
+fruits.forEach(function my(val) {
+  console.log(val);
+});
+
+//callback function//
+let rivers = ["marsyangdi", "kali", "gandaki", "bagmati", "seti", "bheri"];
+rivers.forEach(function (val) {
+  console.log(val);
+});
+
+//arrow//
+let friends = ["ram", "shyam", "krishna", "gita", "aruna"];
+friends.forEach((val) => {
+  console.log(val.replace("ram", "hari"));
+});
+
+let sports = ["volleyball", "football", "table tenis", "chess", "cricket"];
+sports.forEach((val, index) => {
+  console.log(val.toLocaleUpperCase(), index, sports);
+});
+// let sports1 = ["volleyball", "football", "table tenis", "chess", "cricket"];
+// sports.forEach((val) => {
+//   console.log(val.joint("-"));
+// });
+
+//for each using only in string not array
+
+//practice//
+let numbers = [1, 2, 3, 4, 5];
+numbers.forEach((num) => {
+  console.log(num + num);
+});
