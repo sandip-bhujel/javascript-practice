686fbee688085a24cdd9d4b7b32de5c8133850a7
sandip-bhujel <bhujelsandip610@gmail.com>
2025-11-22 21:59:45 +0900

    practicing dom lesson with attribute set get

---
+++ dom-get-set/dom.js
@@ -0,0 +1,22 @@
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let para = document.querySelector("para");
+// console.log(para.getAttribute("class"));
+
+// let head = document.querySelector("head");
+// console.log(head.getAttribute("new"));
+
+//set attribute
+let para = document.querySelector(".para");
+para.style.backgroundColor = "red";
+
+let div = document.querySelector("#box");
+box.style.color = "green";
+
+let h1 = document.querySelector(".new");
+h1.style.color = "blue";
+h1.style.font = "italic";
