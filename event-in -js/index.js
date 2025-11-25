722e3e0e3bc61631cbe2eeaa0b1901cdbe1bdf3f
sandip-bhujel <bhujelsandip610@gmail.com>
2025-11-25 23:24:58 +0900

    this a even in js practice some event handeling

---
+++ event-in -js/index.js
@@ -0,0 +1,22 @@
+//event is in javascript playing very important role we made something and changes any output like button
+//click button submit goes to different show up //
+//many types of event but i try here just famous and importants
+//1// mouse event (click, double click,)
+//2//keyboard event
+
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = () => {
+  console.log("btn1 was clicked");
+  let a = 33;
+  a;
+  a++;
+  console.log(object);
+};
+
+let div = document.querySelector("div");
+box.onmouseover = () => {
+  console.log("you are inside div");
+};
+
+//inline and javascript handeling the good and the  priority is a inside a javascript code//
