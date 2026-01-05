e9794d8bd26466d0a5b19f3c00cc54918bfb9bc0
sandip-bhujel <bhujelsandip610@gmail.com>
2026-01-05 20:18:08 +0900

    practice in javascript classes

---
+++ parentchild-classes/script.js
@@ -0,0 +1,17 @@
+class person {
+  habbit() {
+    console.log("make a good habbit");
+  }
+
+  dicipline() {
+    console.log("dipline make your future");
+  }
+}
+
+class Engineer extends person {
+  work() {
+    console.log("problem solving and coding");
+  }
+}
+
+let sandipbhujel = new Engineer();
